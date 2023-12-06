45292a7942b322fc1e16d0b369eb0ab785252cd4
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-12-05 21:38:23 -0300

    feat: Requisito 6

--- src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,6 +46,7 @@ public class FertilizerController {
    * getAllFertilizers.
    */
   @GetMapping()
+  @Secured({"ROLE_ADMIN"})
   public ResponseEntity<List<FertilizerDto>> getAllFertilizers() {
     List<Fertilizer> fertilizers = fertilizerService.getAllFertilizers();
 
