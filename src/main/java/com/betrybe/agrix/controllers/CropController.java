230438ee77b6029acc61eded448685f2e2d1abc8
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-12-05 21:36:58 -0300

    feat: Requisito 5

--- src/main/java/com/betrybe/agrix/controllers/CropController.java
+++ src/main/java/com/betrybe/agrix/controllers/CropController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +37,7 @@ public class CropController {
    * getAllCrops.
    */
   @GetMapping
+  @Secured({"ROLE_ADMIN", "ROLE_MANAGER"})
   public ResponseEntity<List<CropDto>> getAllCrops() {
     List<Crop> crops = cropService.getAllCrops();
     List<CropDto> cropsDtoList = crops.stream()
